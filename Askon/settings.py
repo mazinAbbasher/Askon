e9c5bbcd400ccbc1f4126ce450a63d5384c90c1f
=mazinAbbasher <=mazinabbayo@gmail.com>
2024-07-06 13:37:11 +0200

    m

--- Askon/settings.py
+++ Askon/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-(u=s33d$4kba5jr*q&av3%rf5xkkrh@tep03&$67)%fx+fp4+r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ["almawa.mazintech.com",'142.93.33.42',"localhost","0.0.0.0","127.0.0.1"]
-ALLOWED_HOSTS = ["almawa.online","www.almawa.online","localhost","0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["almawa.online","www.almawa.online","localhost"]
 
 
 # Application definition
